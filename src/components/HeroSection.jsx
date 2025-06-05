1f937f21db21bf909998983c37d0ba4705a2fa2f
Johan Vargas <johansantiago56@gmail.com>
2025-06-05 16:59:35 -0500

    pruebas

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 const HeroSection = () => {
   return (
     <section
-      className="py-20 md:py-32 bg-cover bg-center relative text-white"
+      className="py-20   md:py-32 bg-cover bg-center relative text-white"
       style={{
         backgroundImage: `linear-gradient(rgba(15, 23, 42, 0.8), rgba(15, 23, 42, 0.8)), 
         url(https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80)`
